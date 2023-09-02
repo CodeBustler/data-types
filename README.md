8663fd98e6d2514c658a0ba0ab1a88f7241a6192
CodeBustler <codebustler360online@gmail.com>
2023-09-02 08:00:18 +0530

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# data-types
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/web-platform-hmbvie)
---
+++ script.js
@@ -0,0 +1,51 @@
+// JS Data Types
+
+// Primitive Data Types ######
+// 1. string | "CodeBustler"
+// 2. number | 10
+// 3. bigint | 123456789012345678901234567890
+// 4. boolean | true | false
+// 5. null
+// 6. undefined
+// 7. Symbol
+
+// Non-premitive Data Types ###### (Reference Data Types )
+// 8. Object | Object, Array, Date, Function, RegExp,
+
+// ####### PREMITIVE #######
+// 1. String
+let str = 'CodeBustler';
+
+// 2. Number
+let num = 2242;
+
+// 3. BigInt
+let x = BigInt('123456789012345678901234567890');
+
+// 4. Boolean
+let a = true;
+let b = false;
+
+// 5. Null (Typeof  Object) | Falsy Values
+let empty = null;
+
+// 6. Symbol
+let symbl = Symbol('description');
+
+// ####### NON-PREMITIVE #######
+
+// 7. Object
+// Array
+let ary = ['a', 13, 41, true];
+
+// Object
+let obj = {
+  name: 'Arjun',
+  age: 29,
+};
+
+// Function | *typeof Function
+let func = (x, y) => x + y;
+
+// 
+
